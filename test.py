bec63e28afe8b55ba64b42fc396b32efe5ca80d9
ArunaCoder <arunacoder@gmail.com>
2022-12-02 15:17:43 -0300

    subindo

--- test.py
+++ test.py
@@ -8,5 +8,54 @@
 # print("Where is my \"cheese\"?")
 # print("Where is my \"cheese\"?")
 
-print(type(True))
-print(type(True))
+# print(type(True))
+# print(type(True))
+
+nome = 'Gabriel'
+sobrenome = 'Silva'
+# idade = 66
+# ano_nascimento = 2022 - idade
+# maior_de_idade = idade >= 18
+# altura_metros = 1.80
+
+# print('Nome:', nome)
+# print('Sobrenome:', sobrenome)
+# print('Idade:', idade)
+# print('Ano de nascimento:', ano_nascimento)
+# print('É maior de idade?', maior_de_idade)
+# print('Altura em metros:', altura_metros)
+
+# divisao_inteira = 10 // 3.0
+# print(divisao_inteira)
+
+# concatenacao = 'A' + ' ' + 'B'
+# a_dez_vezes = 'A' * 10
+
+# print(a_dez_vezes)
+# print(concatenacao)
+
+# altura = 1.95
+# peso = 98
+# imc = peso / altura ** 2
+
+# # print(nome, sobrenome, "tem", altura,"de altura,")
+# # print('pesa', peso,'quilos e seu IMC é')
+
+# # print(imc)
+
+# linha_1 = f'{nome} {sobrenome} tem {altura:.3f} de altura'
+# linha_2 = f'pesa {peso} quilos e seu imc é {imc:.2f}'
+# print(linha_2)
+
+a = 'AAAAA'
+b = 'BBBBBB'
+c = 1.18546
+string = 'b={nome2} a={nome1} a={nome1} c={nome3:.2f}'
+formato = string.format(
+    nome1=a, nome2=b, nome3=c
+)
+# a é argumento, nome1 é parâmetro
+print(formato)
+
+
+
