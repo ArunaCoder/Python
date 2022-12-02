793c1f7bbf60630cb7bff0074a89930cd08e1625
ArunaCoder <arunacoder@gmail.com>
2022-12-01 21:35:17 -0300

    oi

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 #https://www.udemy.com/course/python-3-do-zero-ao-avancado/learn/lecture/34543692#overview
-print('Hello World!')
-print('Outra coisa')
-print('Outra ATENÇÃO')
+# print('Hello World!')
+# print('Outra coisa')
+# print('Outra ATENÇÃO')
+# print(12,34,sep='', end='-')
+# print(12,88,sep='', end='-')
+
+# print(12,999,sep='', end='-')
+
+print(r"Where is my \"cheese\"?")
+print("Where is my \"cheese\"?")
+print("Where is my \"cheese\"?")
