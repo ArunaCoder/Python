34bf5ecd96a45d515c7511ce3a69fa5eb7092d56
ArunaCoder <arunacoder@gmail.com>
2022-12-09 19:01:42 -0300

    ok

---
+++ basic001-020/basic014_interpolacao.py
@@ -0,0 +1,14 @@
+# f string, format e interpolação: usar a de preferência. o luiz usa f string, esta abaixo é interpolação
+
+"""
+Interpolação básica de strings
+s - string
+d e i - int
+f - float
+x e X - Hexadecimal (ABCDEF0123456789)
+"""
+nome = 'Luiz'
+preco = 1000.95897643
+variavel = '%s, o preço é R$%.2f' % (nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %08X' % (1500, 1500))
---
+++ basic001-020/basic016_fatiamento.py
@@ -0,0 +1,15 @@
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd 
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+print(variavel[0:5])
+print(variavel[5:])
+print(variavel[:5])
+print(variavel[-8:-2])
+print(variavel[::-1])
